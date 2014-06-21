e460b94fb6c3e84850ec981ac2db02aad8c5b6e7
Knight Fu <knight.w.fu@gmail.com>
2014-06-20 22:18:00 -0400

    first commit with project initialization

---
+++ src/main/java/HiggsBoson.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: evilwire
+ * Date: 6/20/14
+ * Time: 10:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HiggsBoson {
+}
